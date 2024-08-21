b4516025f78ff3e96128343a01cf610339c99b93
szmulikowski vincent <szmulikowski.vincent.1971@hotmail.com>
2024-08-22 00:41:17 +0200

    $ git push --force-with-lease

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,39 +1,47 @@
-import { render, fireEvent } from "@testing-library/react";
+import { render, fireEvent, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import Counter from "../components/Counter";
+import { act } from "react";
 
 test("le compteur commence à 0, s'incrémente, se décrémente et se réinitialise correctement", () => {
-  const { getByText } = render(
+  render(
     <MemoryRouter>
       <Counter />
     </MemoryRouter>
   );
 
   // Vérifie que le compteur commence à 0
-  const counterValue = getByText("0");
+  const counterValue = screen.getByText("0");
+  expect(counterValue).toBeInTheDocument();
 
   // Bouton d'incrémentation
-  const incrementButton = getByText("+");
-
+  const incrementButton = screen.getByText("+");
   // Bouton de décrémentation
-  const decrementButton = getByText("-");
-
+  const decrementButton = screen.getByText("-");
   // Bouton de réinitialisation
-  const resetButton = getByText("Reset");
+  const resetButton = screen.getByText("Reset");
 
   // Incrémenter le compteur
-  fireEvent.click(incrementButton);
-  expect(getByText("1")).toBeInTheDocument();
+  act(() => {
+    fireEvent.click(incrementButton);
+  });
+  expect(screen.getByText("1")).toBeInTheDocument();
 
   // Décrémenter le compteur
-  fireEvent.click(decrementButton);
-  expect(getByText("0")).toBeInTheDocument();
+  act(() => {
+    fireEvent.click(decrementButton);
+  });
+  expect(screen.getByText("0")).toBeInTheDocument();
 
   // Incrémenter encore pour tester le reset
-  fireEvent.click(incrementButton);
-  expect(getByText("1")).toBeInTheDocument();
+  act(() => {
+    fireEvent.click(incrementButton);
+  });
+  expect(screen.getByText("1")).toBeInTheDocument();
 
   // Réinitialiser le compteur
-  fireEvent.click(resetButton);
-  expect(getByText("0")).toBeInTheDocument();
+  act(() => {
+    fireEvent.click(resetButton);
+  });
+  expect(screen.getByText("0")).toBeInTheDocument();
 });

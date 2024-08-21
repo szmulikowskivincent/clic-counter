00821fec02fb3770c480d011f6811da3db421869
szmulikowski vincent <szmulikowski.vincent.1971@hotmail.com>
2024-08-22 00:02:46 +0200

    $ git push --force-with-lease

--- src/components/error/ErrorPage.js
+++ src/components/error/ErrorPage.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./error-css/ErrorPage.css"; 
+import "./ErrorPage.css"; 
 
 const ErrorPage = () => {
   return (
     <div className="error-page">
-      <h1>Erreur : Le compteur a atteint 10</h1>
+      <h1>⚠️ Erreur : Le compteur a atteint 10</h1>
       <p>Vous avez dépassé la limite autorisée.</p>
       <Link to="/">Retourner à l'accueil</Link>
     </div>
